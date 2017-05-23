e656310491afa21f0fba29b8ee21470ac211803a
prajakta <prajakta@iternia.com>
2017-05-23 16:53:36 +0530

    Initial Commit

--- JCTapButtonSwift/ViewController.swift
+++ JCTapButtonSwift/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelDisplay: UILabel!
+
+    @IBOutlet var buttonReset: UIButton!
+    
+    var currentValue: Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        buttonReset.setTitle("Reset", for:.normal)
+        buttonReset.setTitle("Reseting", for: .highlighted)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonTap(_ sender: UIButton) {
+        
+        let currentValueString: String = labelDisplay.text!
+        
+        currentValue = Int(currentValueString)!
+        
+        currentValue = currentValue + 1
+        
+        let newString = String(currentValue)
+        
+        labelDisplay.text = newString
+        
+        print(newString)
+    }
+    
+    @IBAction func buttonRest(_ sender: UIButton) {
+        
+        labelDisplay.text = "0"
+        
+    }
+    
 
 }
 
